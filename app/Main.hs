be7c745b92061055207d4a8cd5b05fc857374471
Aleksej Kazmin <prSquirrel@users.noreply.github.com>
2016-09-22 20:11:27 +0300

    Add main app logic, some sample programs

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Machine (Meta, Automaton(..), Transition, Symbol, advance)
+import           ConfigParser
+import           ConfigMapper
+import           Data.ByteString as ByteString hiding (putStr)
+import           Control.Arrow
+import           Control.Monad
+import           Data.Maybe
+import           System.Console.ANSI
+import System.Environment (getArgs)
+
 main :: IO ()
-main = print "Hello, world!"
+main = do
+  [fileName, iterations] <- getArgs
+  body <- ByteString.readFile fileName
+  let (meta, automaton, transitions) =
+        case getEnv body of
+          Right a -> a
+          Left e  -> error e
+  let results = runMachine meta (Just automaton) transitions (read iterations :: Int) []
+  forM_ results printTape
+
+printTape :: Automaton -> IO ()
+printTape a = do
+    putStr $ tapeBefore a
+    setSGR [SetUnderlining SingleUnderline]
+    putChar $ headSymbol a
+    setSGR [SetUnderlining NoUnderline]
+    putStr $ tapeAfter a
+    putStr $ "\n\n"
+
+getEnv :: ByteString -> Either String (Meta, Automaton, [Transition])
+getEnv str = readConfig str >>= mapConfig
+  where
+    mapConfig c = left show (fromConfig c)
+
+runMachine :: Meta -> Maybe Automaton -> [Transition] -> Int -> [Automaton] -> [Automaton]
+runMachine _ Nothing _ _ xs = Prelude.reverse xs
+runMachine m (Just a) ts iterations xs
+  | iterations > 0 =
+      let next = advance m a ts
+      in runMachine m next ts (iterations - 1) (a : xs)
+  | otherwise = Prelude.reverse xs
